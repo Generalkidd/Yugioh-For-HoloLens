cf2254b78032dcc4bef6f22ee5a286324044de22
Generalkidd <Plasmadragons@hotmail.com>
2016-08-08 10:13:35 -0400

    Added lifepoints and notification boxes, added player model
    Text can now be displayed on the screen without using OnGUI and there is
    now a player model that will represent the player holographically. Needs
    to be synced over multiplayer though.

---
+++ YuGiOh/Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerModel : MonoBehaviour
+{
+    GameObject g;
+	// Use this for initialization
+	void Start ()
+    {
+        g = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = new Vector3(g.transform.position.x, -1.5f, g.transform.position.z);
+	}
+}
